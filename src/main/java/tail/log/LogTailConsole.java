e2ff110fa2491a84cb0a3d10fba173983bffdc3a
MateusKen <54600427+MateusKen@users.noreply.github.com>
2025-09-17 11:23:05 -0300

    fix: console com menos ifs

--- src/main/java/tail/log/LogTailConsole.java
+++ src/main/java/tail/log/LogTailConsole.java
@@ -3,29 +3,42 @@ package tail.log;
 import static tail.log.FileUtils.*;
 
 public class LogTailConsole {
+
+    enum Mode {
+        FOLLOW, FILTER, REGEX, SHOW, INVALID
+    }
+
     public static void main(String[] args) {
         LogTailArgsParser parser = new LogTailArgsParser(args);
 
-         if (parser.filePath == null) {
-             System.out.println("Log path not provided.");
-             return;
-         }
-
-        if (parser.following) {
-            followingFile(parser.filePath, parser.readAll, parser.wantedLines, parser.filter);
-            return;
+        Mode mode = Mode.SHOW;
+        if (parser.filePath == null) {
+            mode = Mode.INVALID;
+        } else if (parser.following) {
+            mode = Mode.FOLLOW;
+        } else if (parser.filter != null) {
+            mode = Mode.FILTER;
+        } else if (parser.regex != null) {
+            mode = Mode.REGEX;
         }
 
-        if (parser.filter != null) {
-            showFileWithFilter(parser.filePath, parser.readAll, parser.wantedLines, parser.filter);
-            return;
+        switch (mode) {
+            case INVALID:
+                System.out.println("Log path not provided.");
+                break;
+            case FOLLOW:
+                followingFile(parser.filePath, parser.readAll, parser.wantedLines, parser.filter);
+                break;
+            case FILTER:
+                showFileWithFilter(parser.filePath, parser.readAll, parser.wantedLines, parser.filter);
+                break;
+            case REGEX:
+                showFileWithRegex(parser.filePath, parser.readAll, parser.wantedLines, parser.regex);
+                break;
+            case SHOW:
+                showFile(parser.filePath, parser.readAll, parser.wantedLines);
+                break;
         }
-
-        if (parser.regex != null) {
-            showFileWithRegex(parser.filePath, parser.readAll, parser.wantedLines, parser.regex);
-        }
-
-        showFile(parser.filePath, parser.readAll, parser.wantedLines);
     }
 }
 
